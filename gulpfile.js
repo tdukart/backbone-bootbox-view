0cd9091d695ad27cf8f9f59d7f5dec405c105cbd
Todd Dukart <tdukart@aloftinteractive.com>
2015-05-02 14:17:37 -0400

    Improve build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require("gulp");
 var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var uglify = require("gulp-uglify");
 
-gulp.task("default", function () {
+gulp.task("default", function() {
 
   return gulp.src("src/**/*.js")
     // Traces all modules and outputs them in the correct order.
       .pipe(concat("backbone-bootbox-view.js"))
+      .pipe(gulp.dest("dist"))
       .pipe(uglify({
         preserveComments: 'some'
+      }))
+      .pipe(rename(function(path) {
+        if (path.extname === '.js') {
+          path.basename += '.min';
+        }
       }))
       .pipe(gulp.dest("dist"));
 
